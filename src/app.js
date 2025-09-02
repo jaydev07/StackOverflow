bec3191f7db3e7847b8a56bccc765f5c7b77c674
jaydev07 <jdbhavsar213@gmail.com>
2025-09-02 15:47:04 +0530

    Configure CORS to allow requests from localhost with credentials

--- src/app.js
+++ src/app.js
@@ -9,7 +9,10 @@ const answerRoutes = require('./routes/answer-routes');
 const commentRoutes = require('./routes/comment-routes');
 const userRoutes = require('./routes/user-routes');
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
